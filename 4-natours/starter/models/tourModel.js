24a86760e8e7ae5065bbb587eef390e3385019fc
ninomaverick <etengyemmy@gmail.comgit config --global user.email etengyemmy@gmail.com>
2025-03-12 15:36:49 +0100

    Query middleware Mongoose

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -52,9 +52,13 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(),
     select: false
   },
-  startDates: [Date]
-
-}, {
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
+  }
+}, 
+{
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
 });
@@ -66,7 +70,7 @@ tourSchema.virtual('durationWeeks').get(function() {
 // DOCUMENT MIDDLEWARE: only runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
-  next()
+  next();
 });
 
 // tourSchema.pre('save', function(next) {
@@ -78,6 +82,25 @@ tourSchema.pre('save', function(next) {
 //   console.log
 //   next()
 // });
+
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+ this.find({ secretTour: { $ne: true}})
+
+ this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs,next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`)
+  console.log();
+  next();
+})
+
+// AGGREGATION MIDDLEWARE
+
 
 
 const Tour = mongoose.model('Tour', tourSchema);
