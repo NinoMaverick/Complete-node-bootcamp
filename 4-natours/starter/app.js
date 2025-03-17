12ab7cd4840a382c444bad9be9d77ec88a106568
NinoMaverick <etengyemmy@gmail.com>
2025-03-17 17:39:17 +0100

    Error handling (Prod vs Dev)

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -42,13 +42,15 @@ app.all('*', (req, res, next) => {
   next(err);
 });
 
-app.use ((err, req, res, next) => {
+app.use((err, req, res, next) => {
+  //  console.log(err.stack)
+
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error'
+  err.status = err.status || 'error';
 
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message
+    message: err.message,
   });
 });
 
