c0957a0e989e6da5dea44defe7bb88a3e60969fa
ninomaverick <etengyemmy@gmail.comgit config --global user.email etengyemmy@gmail.com>
2025-03-19 17:11:27 +0100

    more errors(uncaught exceptions and unhandled rejections)

--- 4-natours/starter/utils/appError.js
+++ 4-natours/starter/utils/appError.js
@@ -7,22 +7,22 @@ class AppError extends Error {
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
-    Error.caaptureStackTrace(this, this.constructor);
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
 module.exports = AppError;
 
-    constructor(message, statusCode) {
-        super(message);
+//     constructor(message, statusCode) {
+//         super(message);
 
-        this.statusCode = statusCode;
-        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-        this.isOperational = true;
+//         this.statusCode = statusCode;
+//         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+//         this.isOperational = true;
 
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
+//         Error.captureStackTrace(this, this.constructor);
+//     };
+// }
 
-module.exports = AppError;
+// module.exports = AppError;
 
