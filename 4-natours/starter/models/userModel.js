6d47f9f07e2c53c9166eaef59ca04c8dd4d25d0d
ninomaverick <etengyemmy@gmail.comgit config --global user.email etengyemmy@gmail.com>
2025-03-20 09:26:17 +0100

    learnt to hash passwords

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,34 +1,52 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //name, email, photo, password, passwordConfirm
 
-const userSchema = new mongoose.Schema(
- {
-    name: {
-        type: String,
-        required: [true, 'Please tell us your name'],
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password your name'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // this only works on SAVE and CREATE!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
     },
-    email: {
-        type: String,
-        required: [true, 'A user must have an email'],
-        unique: true,
-        lowercase: true,
-        validate: [validator.isEmail, 'Please provide a valid email']
-    },
-    photo: String,
-    password: {
-        type: String,
-        required: [true, 'Please provide a password your name'],
-        minlength: 8
-    },
-    passwordConfirm: {
-        type: String,
-        required: [true, 'Please confirm your password'],
-        minlength: 8
-    }
- });
+  },
+});
 
- const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
 
- module.exports = User;
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION!')
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION!');
   console.log(err.name, err.message);
   server.close(() => {
-    process.exit(1)
+    process.exit(1);
   });
 });
 
-dotenv.config({ path: './config.env' }); 
+dotenv.config({ path: './config.env' });
 const app = require(`./app`);
-
-
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -21,19 +19,19 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true, // Fix old depreciation warning
-    useCreateIndex: true,
-    useFindAndModify: false,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true, // Fix old depreciation warning
+    // useCreateIndex: true,
+    // useFindAndModify: false,
   })
-  .then(() => console.log('DB connection successful'))
-  
+  .then(() => console.log('DB connection successful'));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! 🔥 Shutting down..');
   server.close(() => {
